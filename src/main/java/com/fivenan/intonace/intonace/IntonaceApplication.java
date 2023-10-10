8c93a015276ffa06cb749c388de18321116644f9
Fivenan <limanan.nursalim@gmail.com>
2023-10-10 22:52:29 +0200

    first commit

---
+++ src/main/java/com/fivenan/intonace/intonace/IntonaceApplication.java
@@ -0,0 +1,13 @@
+package com.fivenan.intonace.intonace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntonaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntonaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fivenan/intonace/intonace/IntonaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fivenan.intonace.intonace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntonaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
